421abf1ecf763ed83b36a3b05fbe7b5278289dd4
speed <153678863+ttahaa1@users.noreply.github.com>
2024-02-06 12:02:51 +0200

    ze-telethon-cl.py

--- ze-telethon-cl.py
+++ ze-telethon-cl.py
@@ -82,7 +82,7 @@ bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 DEVELOPER_ID = int(DEVLOO)
 OWNER_ID = DEVELOPER_ID
-developer_id = 5705487207
+developer_id = 6439769452
 days_left = 28
 run = False
 datee = datetime.now()
